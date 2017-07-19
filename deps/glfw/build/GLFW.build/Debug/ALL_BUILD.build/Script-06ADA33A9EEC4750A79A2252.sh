09dbc6f93d6170f7f26f4750b7d7038eb1249b75
Simon Petersson <peterssonaren@gmail.com>
2017-07-18 21:53:39 -0400

    Add CLion project, GLFW and GLAD

---
+++ deps/glfw/build/GLFW.build/Debug/ALL_BUILD.build/Script-06ADA33A9EEC4750A79A2252.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/simonpetersson/Downloads/glfw-3.2.1/build -f /Users/simonpetersson/Downloads/glfw-3.2.1/build/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
---
+++ deps/glfw/build/GLFW.build/Debug/ZERO_CHECK.build/Script-63C5DF92276843B193F62785.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/simonpetersson/Downloads/glfw-3.2.1/build -f /Users/simonpetersson/Downloads/glfw-3.2.1/build/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
